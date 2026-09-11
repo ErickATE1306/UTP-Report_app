53ce90213015951922db44957ba77cb8752d8443
AlvaroAlexisCH <alvaroa.capcha@gmail.com>
2026-09-11 18:20:15 -0500

    vista incial de super

--- UTP+Report_copy/front/src/app/app.routes.ts
+++ UTP+Report_copy/front/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { adminGuard } from './core/auth/admin.guard';
 import { LoginComponent } from './features/auth/login/login.component';
+import { Inicio as SuperAdminInicio } from './SuperAdmin/inicio/inicio';
 
 export const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
 	{ path: 'admin', canActivate: [adminGuard], loadChildren: () => import('./features/admin/admin.module').then((module) => module.AdminModule) },
+	{ path: 'superadmin/dashboard/:tab', component: SuperAdminInicio },
+	{ path: 'superadmin/dashboard', component: SuperAdminInicio },
 	{ path: '', pathMatch: 'full', redirectTo: 'admin' },
 	{ path: '**', redirectTo: 'admin' }
+
 ];
