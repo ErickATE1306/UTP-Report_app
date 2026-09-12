52238fa875271e2fe75c87da4001ff181f005011
AlvaroAlexisCH <alvaroa.capcha@gmail.com>
2026-09-11 19:09:39 -0500

    limpiar copia web

--- UTPReportWeb/front/src/app/constants/roles.ts
+++
@@ -1,9 +0,0 @@
-export const ROLES = {
-  SUPERADMIN: 'ROLE_SUPERADMIN',
-  ADMIN: 'ROLE_ADMIN',
-  USUARIO: 'ROLE_USUARIO',
-  SEGURIDAD: 'ROLE_SEGURIDAD'
-} as const;
-
-export type RoleKey = keyof typeof ROLES;
-export const ALL_ROLES: string[] = Object.values(ROLES);
